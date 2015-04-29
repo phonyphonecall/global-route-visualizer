ad910aa6ef77dc04484857320d023c462ae27b69
Scott Hendrickson <phonyphonecall@aol.com>
2015-04-28 21:36:34 -0500

    svg dumping

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 int gx, gy;
 int totalEdges;
+int capacity;
 int* usages;
 
 using namespace std;
@@ -17,6 +18,23 @@ int get_horizontal_right_edge(int x, int y) {
 
 int get_vertical_upper_edge(int x, int y) {
     return (gx - 1) * gy + (y * gx) + x;
+}
+
+void getSegmentFromEdge(int edgeId, int& x1, int& y1, int& x2, int& y2) {
+    int totalHorizEdges =  (gx - 1) * gy;
+    if (edgeId < totalHorizEdges) {
+        // horizontal edge
+        x1 = edgeId % (gx - 1);
+        y1 = (edgeId) / (gx - 1);
+        x2 =  x1 + 1;
+        y2 =  y1;
+    } else {
+        // vertical edge
+        x1 = (edgeId - totalHorizEdges) % (gx);
+        y1 = ((edgeId - totalHorizEdges) - x1) / gx;
+        x2 =  x1;
+        y2 =  y1 + 1;
+    }
 }
 
 int getEdgeId(int x1, int y1, int x2, int y2) {
@@ -49,7 +67,6 @@ void getEdges(int x1, int y1, int x2, int y2, unordered_set<int>& edges) {
         printf("<< ERROR: Invalid edge(s) in solution file: (%d,%d)-(%d,%d)\n", x1, y1, x2, y2);
         exit(-1);
     }
-//    printf("(%d,%d)-(%d,%d) has %d edges\n", x1, y1, x2, y2, edges.size());
 }
 
 int main(int argc, char **argv) {
@@ -65,12 +82,11 @@ int main(int argc, char **argv) {
 
 	string inputFilename = argv[1];
 	string solutionFilename = argv[2];
- 	string outputFilename = argv[3];
+ 	char* outputFilename = argv[3];
 
  	// read benchmark
     ifstream in(inputFilename);
     if (in.is_open()) {
-        int capacity;
         int numNets;
         int numBlockages = -1;
         getline(in,line);
@@ -118,7 +134,6 @@ int main(int argc, char **argv) {
         unordered_set<int> netEdges;
         while(getline(soln, line)) {
             if (line[0] == '!') {
-//                printf("net has %d edges\n", netEdges.size());
                 for (int edge : netEdges) {
                     usages[edge]--;
                 }
@@ -134,12 +149,54 @@ int main(int argc, char **argv) {
 
     int tof = 0;
     int ovf = 0;
+    int maxOverflow = 1;   // max usage of overflowed edge
     for (int i = 0; i < totalEdges; i++) {
         ovf = usages[i];
         if (ovf < 0) {
             tof += abs(ovf);
+            if (abs(ovf) > maxOverflow)
+                maxOverflow = abs(ovf);
         }
     }
+
+    // build svg
+    //  use old c files for fprintf convenience
+    FILE* f = fopen(outputFilename, "w");
+    fprintf(f, "<!DOCTYPE html><html><body>\n");
+    fprintf(f, "<p><h3>Total Overflow: %d</h3></p>\n", tof);
+    fprintf(f, "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" style='width:%ipx;height:%ipx'>\n", gx * 10, gy * 10);
+
+    for (unsigned int i = 0; i < gx; i++) {
+        for (unsigned int j = 0; j < gy; j++) {
+            fprintf(f, "<circle cx=\"%d\" cy=\"%d\" r=\"1\" stroke=\"black\" stroke-width=\"0\" fill=\"grey\" />\n", i*10, j*10);
+        }
+    }
+
+    int usage;
+    int r, g, b, width;
+    for (int i = 0; i < totalEdges; i++) {
+        usage = usages[i];
+        if (usage == capacity) {
+            continue;
+        } else if (usage > 0) {
+            // non-overflow case... grey. darker == more usage
+            r = int(225.0 * (double(usage) / double(capacity)));
+            g = r;
+            b = r;
+            width = 7 - int(7.0 * (double(usage) / double(capacity)));
+        } else {
+            // overlfow... red. darker == more usage
+//            r = int(245.0 * (double(abs(usage)) / double(maxOverflow)));
+            r = 255;
+            g = 0;
+            b = 0;
+            width =  7 - int(7.0 * (double(abs(usage)) / double(maxOverflow)));
+        }
+        int x1, y1, x2, y2;
+        getSegmentFromEdge(i, x1, y1, x2, y2);
+        fprintf(f, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" style=\"stroke:rgb(%d,%d,%d);stroke-width:%d\" />\n", 10 * x1, 10*y1, 10*x2, 10*y2, r, g, b, width);
+    }
+
     printf("total overflow: %d\n", tof);
  	return 0;
 }
